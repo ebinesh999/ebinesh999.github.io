6fd8fd5b3269ef3fd2f2472f065add06c2924d7b
Ebinesh M <128308565+ebinesh999@users.noreply.github.com>
2023-10-26 23:59:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('a');
+}
+
+//scorall section
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']'). classList.add('active');
+            });
+        }
+});
+
+
+
+     let header = document.querySelector('header');
+
+     header.classList.toggle('sticky',window.scrollY > 100);
+}
